f986dda7d96cec8a02b5f3955fdf95edbc155dad
daniel <bangseongmin@naver.com>
2024-09-20 11:35:09 +0900

    #2 - 대출 신청 서류 조회 기능 구현
    현재 파일과 대류 신청을 매핑하지 않았기 때문에 간단하게 파일을 조회하고, 파일에 대한 URL 생성하는 과정으로 마무리함.

--- src/main/java/org/example/loan/service/FileStorageService.java
+++ src/main/java/org/example/loan/service/FileStorageService.java
@@ -1,11 +1,17 @@
 package org.example.loan.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public interface FileStorageService {
 
-    void save(MultipartFile file) throws IOException;
+    void save(MultipartFile file);
+
+    Resource load(String filename);
+
+    Stream<Path> loadAll();
 
 }
--- src/main/java/org/example/loan/service/FileStorageServiceImpl.java
+++ src/main/java/org/example/loan/service/FileStorageServiceImpl.java
@@ -4,13 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.example.loan.exception.BaseException;
 import org.example.loan.exception.ResultType;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +32,33 @@ public class FileStorageServiceImpl implements FileStorageService {
                     Paths.get(uploadPath).resolve(Objects.requireNonNull(file.getOriginalFilename())),
                     StandardCopyOption.REPLACE_EXISTING
             );
+        } catch (Exception e) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = Paths.get(uploadPath).resolve(filename);
+
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new BaseException(ResultType.FILE_NOT_EXIST);
+            }
+
+        } catch (Exception e) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(Paths.get(uploadPath), 1).filter(path -> !path.equals(Paths.get(uploadPath)));
         } catch (Exception e) {
             throw new BaseException(ResultType.SYSTEM_ERROR);
         }
