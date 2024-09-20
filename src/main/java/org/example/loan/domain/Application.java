beb0d95180610ff9c514be3848b1cfb9a7837993
daniel <bangseongmin@naver.com>
2024-09-20 15:55:39 +0900

    #4 - 대출 계약 기능 구현

--- src/main/java/org/example/loan/domain/Application.java
+++ src/main/java/org/example/loan/domain/Application.java
@@ -51,5 +51,8 @@ public class Application extends BaseEntity {
 
     @Column(columnDefinition = "decimal(15,2) DEFAULT NULL COMMENT '승인 금액'")
     private BigDecimal approvalAmount;
+
+    @Column(columnDefinition = "datetime DEFAULT NULL COMMENT '약정일자'")
+    private LocalDateTime contractedAt;
 }
 
