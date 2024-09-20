85b9172a07a5534da1327ab3c345b25ddc687b25
Elian <bangseongmin@naver.com>
2024-09-20 12:16:01 +0900

    #2 - 대출 신청 기능 개발 (#7)
    * 대출 신청 도메인 테이블 정의
    *  대출 신청 등록 기능 구현
    * 대출 신청 조회 기능 구현
    * 대출 신청 수정 기능 구현
    * 대출 신청 삭제 기능 구현
    * 이용 약관 등록 및 조회 기능 구현
    * 대출 신청 이용 약관 등록 기능 구현
    * 대출 신청 서류 등록 기능 구현
    * 대출 신청 서류 조회 기능 구현
    * 대출 신청 서류 삭제 기능 구현
    * 대출 신청 서류 매핑 기능 구현

---
+++ src/main/java/org/example/loan/repository/TermsRepository.java
@@ -0,0 +1,9 @@
+package org.example.loan.repository;
+
+import org.example.loan.domain.Terms;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TermsRepository extends JpaRepository<Terms, Long> {
+}
---
+++ src/main/java/org/example/loan/service/ApplicationServiceImpl.java
@@ -0,0 +1,113 @@
+package org.example.loan.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.loan.domain.Application;
+import org.example.loan.domain.Terms;
+import org.example.loan.dto.ApplicationDTO;
+import org.example.loan.exception.BaseException;
+import org.example.loan.exception.ResultType;
+import org.example.loan.repository.AcceptTermsRepository;
+import org.example.loan.repository.ApplicationRepository;
+import org.example.loan.repository.TermsRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.example.loan.dto.ApplicationDTO.*;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicationServiceImpl implements ApplicationService {
+
+    private final ApplicationRepository applicationRepository;
+
+    private final TermsRepository termsRepository;
+    ;
+
+    private final ModelMapper modelMapper;
+    private final AcceptTermsRepository acceptTermsRepository;
+
+    @Override
+    public Response create(Request request) {
+        Application application = modelMapper.map(request, Application.class);
+        application.setAppliedAt(LocalDateTime.now());
+
+        Application applied = applicationRepository.save(application);
+
+        return modelMapper.map(applied, Response.class);
+    }
+
+    @Override
+    public Response get(Long applicationId) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        return modelMapper.map(application, Response.class);
+    }
+
+    @Override
+    public Response update(Long applicationId, Request request) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        application.setName(request.getName());
+        application.setCellPhone(request.getCellPhone());
+        application.setEmail(request.getEmail());
+        application.setHopeAmount(request.getHopeAmount());
+
+        applicationRepository.save(application);
+
+        return modelMapper.map(application, Response.class);
+    }
+
+    @Override
+    public void delete(Long applicationId) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        application.setIsDeleted(true);
+
+        applicationRepository.save(application);
+    }
+
+    @Override
+    public Boolean acceptTerms(Long applicationId, AcceptTerms request) {
+        applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        List<Terms> termsList = termsRepository.findAll(Sort.by(Sort.Direction.ASC, "termsId"));
+
+        if (termsList.isEmpty()) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        List<Long> acceptTermsIds = request.getAcceptTermsIds();
+        if (termsList.size() != acceptTermsIds.size()) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        List<Long> termsIdes = termsList.stream().map(Terms::getTermsId).toList();
+        Collections.sort(acceptTermsIds);
+
+        if(!new HashSet<>(termsIdes).containsAll(acceptTermsIds)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        for (Long termId : acceptTermsIds) {
+            org.example.loan.domain.AcceptTerms accepted = org.example.loan.domain.AcceptTerms.builder()
+                    .termsId(termId)
+                    .applicationId(applicationId)
+                    .build();
+
+            acceptTermsRepository.save(accepted);
+        }
+
+        return true;
+    }
+
+}
